70831d3a11906abb4ee6f57606f689bddd6df796
noorzaka <nooralhuda101020@gmail.com>
2024-06-22 02:35:48 +0300

    Adding Transfar function

--- BankAccount.java
+++ BankAccount.java
@@ -35,6 +35,23 @@ public class BankAccount {
     public String getAccountHolder() {
         return accountHolder;
     }
+ public void transfer(BankAccount toAccount, double amount) {
+        if (amount > this.balance) {
+            System.out.println("Insufficient funds");
+        } else {
+            this.withdraw(amount);
 
+            double convertedAmount = convertCurrency(amount, this.currency, toAccount.getCurrency());
+            toAccount.deposit(convertedAmount);
+        }
+    }
+
+    private double convertCurrency(double amount, String fromCurrency, String toCurrency) {
+        // Example conversion rates
+        double conversionRate = 1.0;
+        if (fromCurrency.equals("USD") && toCurrency.equals("EUR")) {
+            conversionRate = 0.85;
+        } else if (fromCurrency.equals("USD") && toCurrency.equals("GBP")) {
+            conversionRate =
 
 }
